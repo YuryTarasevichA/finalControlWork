2921b7b5688fdfb2aabe22256b5cfdff58da1326
Yura <yura.tarasevichunt@gmail.com>
2023-07-23 11:08:21 +0300

    codnapisan

--- program/Program.cs
+++ program/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// string [] array = new string[] {"string1", "string2", "string3"};
+
+void PrintArrStr(string [] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+void PrintNewArray(string [] arr)
+{
+    Console.Write("[");
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            if (i < arr.Length - 1) Console.Write($"{arr[i]}, ");
+            else Console.Write($"{arr[i]}");
+            count++;
+        }
+    }
+    Console.Write("]");
+}
+Console.WriteLine("Введите строки через пробел: ");
+string input = Console.ReadLine();
+string [] array = input.Split(' ');
+PrintArrStr(array);
+Console.WriteLine();
+PrintNewArray(array);
+
